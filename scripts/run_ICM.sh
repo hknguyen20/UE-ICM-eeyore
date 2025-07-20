fdf9551987e966da607957cc1f67518dec451340
Nguyen Hoang <20nguyen.hk@vinuni.edu.vn>
2025-07-20 03:37:06 +0000

    chore: update batch_size due to max_model_len limitation, add log

--- scripts/run_ICM.sh
+++ scripts/run_ICM.sh
@@ -9,6 +9,7 @@ nohup python ICM.py \
   --model hugging-quants/Meta-Llama-3.1-8B-Instruct-AWQ-INT4 \
   --K 100 \
   --consistency_fix_K 5 \
+  --batch_size 32 \
   > "progress.log" 2>&1 &
 
 echo "Process started with PID: $!"
